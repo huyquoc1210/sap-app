d467cb36f1a9da8ef39d947daaf34944fd7f49e2
huyquoc1210 <contact.huyquoc@gmail.com>
2024-12-11 11:23:54 +0700

    lỗi syntax

---
+++ src/utli/Helper.ts
@@ -0,0 +1,10 @@
+const Helper = {
+  resolvePath: function (imgPath: string): string {
+    const rootPath = (sap as any).ui.require.toUrl(
+      "sap/m/sample/TsTodos/webapp"
+    );
+    return rootPath + imgPath;
+  },
+};
+
+export default Helper;
