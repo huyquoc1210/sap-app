418fecc0826c2eb708b49c0bbbde0bc6f9195a46
huyquoc1210 <contact.huyquoc@gmail.com>
2024-12-11 14:24:04 +0700

    render index.html

---
+++ webapp/util/Helper.js
@@ -0,0 +1,12 @@
+sap.ui.define([], function () {
+  "use strict";
+
+  var Helper = {
+    resolvePath: function resolvePath(imgPath) {
+      var rootPath = sap.ui.require.toUrl("sap/m/sample/TsTodos/webapp");
+      return rootPath + imgPath;
+    }
+  };
+  return Helper;
+});
+//# sourceMappingURL=Helper.js.map
